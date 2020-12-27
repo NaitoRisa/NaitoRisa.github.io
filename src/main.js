80757f45d35425f8d9bb4712c2ed30252875fbdc
内藤理紗 <naitourisa@naitourisanoMacBook-Pro.local>
2020-12-28 00:07:46 +0900

    :art: TOPページのフォーマット完成

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
 // scssを追加する場合はここに書いていく
 import './scss/style.scss';
-import './images/about_main.png';
+
+// TOPページ画像
+import './images/about/about_title.png';
+import './images/about/about_main.png';
+import './images/about/about_card.png';
+import './images/about/about_sauna.png';
+import './images/about/about_onigiri.png';
+import './images/about/about_goodmorning.png';
+import './images/about/about_copy.png';
+import './images/about/about_instagram.png';
